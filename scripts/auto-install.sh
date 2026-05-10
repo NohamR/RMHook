35423e1c1a459da9576f63be2f0fe64e7626c2f5
√(noham)² <100566912+NohamR@users.noreply.github.com>
2026-05-10 16:30:04 +0200

    Update auto-install.sh

--- scripts/auto-install.sh
+++ scripts/auto-install.sh
@@ -3,21 +3,26 @@ REPO="NohamR/RMHook"
 FILE="rmfakecloud.dylib"
 APP_PATH="/Applications/remarkable.app"
 
-curl -L \
+echo "[INFO] Downloading $FILE..."
+curl -sL \
   -o "/tmp/$FILE" \
   "https://github.com/$REPO/releases/latest/download/$FILE"
 
 # Fix the sandbox
-ln -s ~/Library/Containers/com.remarkable.desktop/Data/Library/Application\ Support/remarkable \
+echo "[INFO] Linking sandbox directory..."
+ln -sf ~/Library/Containers/com.remarkable.desktop/Data/Library/Application\ Support/remarkable \
       ~/Library/Application\ Support/remarkable
 
-curl -L \
+echo "[INFO] Downloading inject script..."
+curl -sL \
   -o "/tmp/inject.sh" \
   "https://raw.githubusercontent.com/$REPO/refs/heads/main/scripts/inject.sh"
 
-curl -L \
+echo "[INFO] Downloading optool..."
+curl -sL \
   -o "/tmp/optool" \
   "https://raw.githubusercontent.com/$REPO/refs/heads/main/scripts/optool"
 
-chmod +x /tmp/inject.sh
+chmod +x /tmp/inject.sh /tmp/optool
+echo "[INFO] Running inject script..."
 /tmp/inject.sh "/tmp/$FILE" "$APP_PATH"
